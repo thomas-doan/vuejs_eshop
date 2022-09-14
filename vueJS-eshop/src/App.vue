e635da1ae3bd7472793cd5460c03a3eac1f0dd4c
doan <thomas.doan@laplateforme.io>
2022-09-14 14:58:02 +0200

    pratique vuejs, liaison des classes

---
+++ vueJS-eshop/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <input
+      class="input"
+      :class="{
+        inputOnGoing: inputOnGoing,
+      }"
+      type="text"
+      v-model="input"
+    />
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+const input = ref("");
+const inputOnGoing = computed(() => input.value.length);
+</script>
+
+<style scoped>
+.input {
+  outline: 0;
+  border: 1px solid black;
+  border-radius: 4px;
+}
+
+.inputOnGoing {
+  border-color: blue;
+}
+</style>
