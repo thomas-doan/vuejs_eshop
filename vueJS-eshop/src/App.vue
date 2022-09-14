ded667f013f123d74017253b0b810c09424fbf26
doan <thomas.doan@laplateforme.io>
2022-09-14 15:33:20 +0200

    ajout de l'erreur

--- vueJS-eshop/src/App.vue
+++ vueJS-eshop/src/App.vue
@@ -4,7 +4,12 @@
       class="input"
       :class="{
         inputOnGoing: inputOnGoing,
+
+        //synthaxe plus rapide
+        inputError,
       }"
+      @focus="focus = true"
+      @blur="focus = false"
       type="text"
       v-model="input"
     />
@@ -20,8 +25,13 @@ import { RouterLink, RouterView } from "vue-router";
 //ref permet de la réactivité uniquement sur des primitives
 const input = ref("");
 
+//permet d'écouter le focus ou pas et en fonction assigné une nouvelle classe
+const focus = ref(false);
+
 //computed permet d'actualiser
-const inputOnGoing = computed(() => input.value.length);
+const inputOnGoing = computed(() => focus.value && input.value.length);
+
+const inputError = computed(() => input.value.length > 6);
 </script>
 
 <style scoped>
@@ -33,5 +43,9 @@ const inputOnGoing = computed(() => input.value.length);
 
 .inputOnGoing {
   border-color: blue;
+}
+
+.inputError {
+  border-color: red;
 }
 </style>
